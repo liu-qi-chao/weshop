3ed3e02421ad557a2d9d375e67addf9246f7056c
qc <1507071410@qq.com>
2020-02-18 18:25:30 +0800

    gengxin

--- src/components/FooterNav/FooterNav.vue
+++ src/components/FooterNav/FooterNav.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="footerWrap">
-    <div @click="goPath('/home')" class="navItem">
+    <div :class="{active: $route.path === '/home'}" @click="goPath('/home')" class="navItem">
       <i class="iconfont icon-home"></i>
       <span>首页</span>
     </div>
-    <div @click="goPath('/sort')" class="navItem">
+    <div :class="{active: $route.path === '/sort'}" @click="goPath('/sort')" class="navItem">
       <i class="iconfont icon-fenlei"></i>
       <span>分类</span>
     </div>
-    <div @click="goPath('/buy')" class="navItem">
+    <div :class="{active: $route.path === '/buy'}" @click="goPath('/buy')" class="navItem">
       <i class="iconfont icon-zhidemai"></i>
       <span>值得买</span>
     </div>
-    <div @click="goPath('/cart')" class="navItem">
+    <div :class="{active: $route.path === '/cart'}" @click="goPath('/cart')" class="navItem">
       <i class="iconfont icon-gouwuche"></i>
       <span>购物车</span>
     </div>
-    <div @click="goPath('/person')" class="navItem">
+    <div :class="{active: $route.path === '/person'}" @click="goPath('/person')" class="navItem">
       <i class="iconfont icon-gerenzhongxin"></i>
       <span>个人</span>
     </div>
@@ -28,35 +28,37 @@ export default {
   methods: {
     goPath(path) {
       //跳转至指定的路径
-      this.$router.push(path);
+      path !== this.$route.path && this.$router.push(path);
     }
   }
 };
 </script>
-
-<style scoped lang='stylus'>
+<style  lang='stylus'>
+ @import "../../common/stylus/mixins.styl"
 #footerWrap 
   display: flex;
+  top-border-1px(#eee)
   position: fixed;
   left: 0;
   bottom: 0;
   width: 100%;
-  height: 98px;
-  border-top 1px 
   box-sizing border-box
   background #fff
   .navItem 
     width: 20%;
     height: 98px;
     text-align: center;
-    padding-top 20px
+    // line-height 40px
+    // padding-top 10px
     display flex
     flex-direction column
+    font-size 24px
     &.active
       color red
     .iconfont
       font-size 42px
-      line-height 30px
+      line-height 60px
+
  
 
 </style>
