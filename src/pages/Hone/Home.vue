2e7e2ff75a65eb9262b3f63527d65881040e8a9a
qc <1507071410@qq.com>
2020-02-19 14:29:15 +0800

    update

--- src/pages/Hone/Home.vue
+++ src/pages/Hone/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="wrap">
+  <div id="wrapper">
     <div class="header">
         <img src="../../common/images/logo.png" alt />
       <div class="search">
@@ -8,7 +8,7 @@
       </div>
       <div class="login">登录</div>
     </div>
-    <div class="listWrap">
+    <div class="listContainer">
      <ul class="navList">
            <li>推荐</li>
            <li>居家生活</li>
@@ -24,22 +24,126 @@
           <i class="iconfont icon-Icon_ArrowDown"></i>
       </div>
     </div>
+    <!-- 轮播图 -->
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <img src="../../common/images/swiper/01.webp" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="../../common/images/swiper/2.webp" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="../../common/images/swiper/3.webp" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="../../common/images/swiper/4.webp" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="../../common/images/swiper/5.webp" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="../../common/images/swiper/6.webp" alt="">
+          </div>
+          <div class="swiper-slide">
+            <img src="../../common/images/swiper/7.webp" alt="">
+          </div>
+        </div> 
+        <div class="swiper-pagination">
+          <span class="swiper-pagination-bullet"></span>
+        </div>
+      </div>
+      <!-- 列表 -->
+      <div class="bList">
+            <div>
+              <i class="iconfont icon-wangyi"></i>
+              <span>网易自营品牌</span>
+            </div>
+            <div>
+              <i class="iconfont icon-tianwuyou"></i>
+              <span>30天无忧退货</span>
+            </div>
+            <div>
+              <i class="iconfont icon-tuikuan"></i>
+              <span>48小时极速退款</span>
+            </div>
+      </div>
+      <!-- 商品列表 -->
+      <div class="shopList-container">
+        <div class="shop-list">
+          <ul class="shopL">
+            <li>
+              <img src="../../common/images/swiper/8.png" alt="">
+              <span>新品首发</span>
+            </li>
+            <li>
+              <img src="../../common/images/swiper/8.png" alt="">
+              <span>新品首发</span>
+            </li>
+            <li>
+              <img src="../../common/images/swiper/8.png" alt="">
+              <span>新品首发</span>
+            </li>
+            <li>
+              <img src="../../common/images/swiper/8.png" alt="">
+              <span>新品首发</span>
+            </li>
+            <li>
+              <img src="../../common/images/swiper/8.png" alt="">
+              <span>新品首发</span>
+            </li>
+          </ul>
+          <ul class="shopL">
+            <li>
+              <img src="../../common/images/swiper/8.png" alt="">
+              <span>新品首发</span>
+            </li>
+            <li>
+              <img src="../../common/images/swiper/8.png" alt="">
+              <span>新品首发</span>
+            </li>
+            <li>
+              <img src="../../common/images/swiper/8.png" alt="">
+              <span>新品首发</span>
+            </li>
+            <li>
+              <img src="../../common/images/swiper/8.png" alt="">
+              <span>新品首发</span>
+            </li>
+            <li>
+              <img src="../../common/images/swiper/8.png" alt="">
+              <span>新品首发</span>
+            </li>
+          </ul>
+        </div>
+      </div>
   </div>
+  
 </template>
 
 <script type="text/ecmascript-6">
-import BScroll from 'better-scroll'
+import BScorll from 'better-scroll'
+import Swiper from "swiper";
+import "swiper/css/swiper.min.css";
   export default {
     mounted() {
-      new BScroll('.listWrap',{
+      new BScorll('.listContainer',{
             scrollX:true
-          })
+         })
+          new Swiper(".swiper-container", {
+            autoplay: true,//自动轮播
+            pagination: {
+              el: ".swiper-pagination"
+            },
+            loop: true
+          });
+          
     },
   }
 </script>
 
 <style scoped lang='stylus'>
-#wrap 
+#wrapper 
   .header 
     width: 100%;
     height: 88px;
@@ -77,13 +181,13 @@ import BScroll from 'better-scroll'
       text-align center
       line-height 40px
       margin-left 16px
-  .listWrap
+  .listContainer
     position relative
     height 60px
     line-height 60px
     display flex
     overflow hidden
-	.navList
+    .navList
       display flex
       white-space nowrap
       font-size 28px
@@ -102,16 +206,76 @@ import BScroll from 'better-scroll'
           width 100%
           height 4px
           background #dd1a21  
-    .btn
-      width 100px
-      height 60px
-      background #fff
-     
-      position absolute
-      right 0  
+     .btn
+       width 100px
+       height 60px
+       background #fff
+       display flex
+       position absolute
+       right 0  
+       .iconfont 
+         width 100px
+         height 60px
+         text-align center
+         line-height 60px
+         font-size 42px 
+  .swiper-container
+    width 100%
+    height 370px
+    .swiper-wrapper
+      width 100%
+      height 370px
+      .swiper-slide
+        width 100%
+        height 370px
+        img 
+          width 100%
+          height 370px
+    /deep/.swiper-pagination
+     .swiper-pagination-bullet
+        width 40px
+        height 4px
+        background #fff
+        border-radius 0
+  .bList
+    width 100%
+    height 72px
+    display flex
+    padding 0 30px
+    box-sizing border-box
+    align-items center
+    div
+      width 228px
+      height 36px
+      display flex
+      align-items center
       i 
-        line-height 60px
-        text-align center
-        font-size 32px  
-        color red   
+        font-size 32px
+        color #dd1a21 
+      span
+        font-size 24px 
+        padding-left 8px
+        0           
+  .shopList-container
+    width 100%
+    height 373px
+    .shop-list
+      width 100%
+      height 341px
+      .shopL
+        display flex
+        li 
+          width 20%
+          margin 10px 20px
+          img 
+            width 110px
+            height 110px
+            margin-bottom 10px
+          span 
+            display block
+            width 110px
+            height 36px
+            line-height 30px
+            font-size 24px 
+            text-align center           
 </style>
